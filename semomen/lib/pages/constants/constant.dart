bcb1fb4e55314e44163d2f496bda640b069ea37f
jspark2201 <jspark2201@gmail.com>
2022-03-27 20:26:35 +0900

    Create app structure

---
+++ semomen/lib/pages/constants/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// color
+Color? mainColor;
+
+// font size
---
+++ semomen/lib/pages/root_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:semomen/pages/chat/chat_page.dart';
+import 'package:semomen/pages/home/home_page.dart';
+import 'package:semomen/pages/profile/profile_page.dart';
+import 'package:semomen/pages/search/search_page.dart';
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  _RootPageState createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  final List<Widget> _pages = [
+    HomePage(),
+    SearchPage(),
+    ChatPage(),
+    ProfilePage(),
+  ];
+  int _selectedIndex = 0;
+
+  Widget _bottomNavigationBar(int selectedIndex) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        onTap: (int index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+          BottomNavigationBarItem(icon: Icon(Icons.chat), label: '채팅'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '프로필'),
+        ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: _bottomNavigationBar(_selectedIndex),
+    );
+  }
+}
