8e8b0cc36d200403536945b70fb72aa2a91585a9
jspark2201 <jspark2201@gmail.com>
2022-03-29 16:23:23 +0900

    Add main colors in constant

--- semomen/lib/pages/constants/constant.dart
+++ semomen/lib/pages/constants/constant.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 // color
-Color? mainColor;
+Color mainNavyBlue = Color(0xff05445e);
+Color mainBlueGrotto = Color(0xff189ab4);
+Color mainBlueGreen = Color(0xff75e6da);
+Color mainBabyBlue = Color(0xffd4f1f4);
 
 // font size
