e2ffa6da5c49101a546a0f699a7fd9953de58ec0
김형표 <gimhyeongpyo@gimhyeongpyoui-MacBookPro.local>
2022-03-29 21:56:28 +0900

    Create purchased guide page

--- semomen/lib/pages/profile/profile_page.dart
+++ semomen/lib/pages/profile/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:semomen/pages/my_coupon_page.dart';
+import 'package:semomen/pages/purchased_guide_page.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -22,7 +24,13 @@ class _ProfilePageState extends State<ProfilePage> {
                   ListTile(
                     leading: Text('구매 가이드'),
                     trailing: IconButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => PurchasedGuidePage()),
+                        );
+                      },
                       icon: Icon(Icons.chevron_right),
                     ),
                     dense: true,
@@ -33,7 +41,13 @@ class _ProfilePageState extends State<ProfilePage> {
                   ListTile(
                     leading: Text('쿠폰함'),
                     trailing: IconButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => MyCouponPage()),
+                        );
+                      },
                       icon: Icon(Icons.chevron_right),
                     ),
                     dense: true,
@@ -52,78 +66,85 @@ class _ProfilePageState extends State<ProfilePage> {
 
   Widget _signOutBox() {
     return Card(
-            child: ListTile(
-              leading: Text('로그아웃', style: TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.chevron_right),
-              ),
-              dense: true,
-            ),
-          );
+      child: ListTile(
+        leading: Text(
+          '로그아웃',
+          style:
+              TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold),
+        ),
+        trailing: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.chevron_right),
+        ),
+        dense: true,
+      ),
+    );
   }
 
   Widget _settingsBox() {
     return Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Text('계정 관리'),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.chevron_right),
-                  ),
-                  dense: true,
-                ),
-                Divider(
-                  height: 0.0,
-                ),
-                ListTile(
-                  leading: Text('고객 센터'),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.chevron_right),
-                  ),
-                  dense: true,
-                ),
-                Divider(
-                  height: 0.0,
-                ),
-                ListTile(
-                  leading: Text('환경 설정'),
-                  trailing: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.chevron_right),
-                  ),
-                  dense: true,
-                ),
-              ],
+      child: Column(
+        children: [
+          ListTile(
+            leading: Text('계정 관리'),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.chevron_right),
             ),
-          );
+            dense: true,
+          ),
+          Divider(
+            height: 0.0,
+          ),
+          ListTile(
+            leading: Text('고객 센터'),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.chevron_right),
+            ),
+            dense: true,
+          ),
+          Divider(
+            height: 0.0,
+          ),
+          ListTile(
+            leading: Text('환경 설정'),
+            trailing: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.chevron_right),
+            ),
+            dense: true,
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _profileBox(Size size) {
     return SizedBox(
-            height: size.height * 0.3,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: size.width * 0.1,
-                  backgroundImage: NetworkImage("https://cdn.pixabay.com/photo/2018/04/27/03/50/portrait-3353699_1280.jpg"),
-                  backgroundColor: Colors.transparent,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Text('name', style: TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold),),
-                ),
-                ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Color(0xff05445e)
-                    ),
-                    onPressed: () {}, child: Text('프로필 수정')),
-              ],
+      height: size.height * 0.3,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: size.width * 0.1,
+            backgroundImage: NetworkImage(
+                "https://cdn.pixabay.com/photo/2018/04/27/03/50/portrait-3353699_1280.jpg"),
+            backgroundColor: Colors.transparent,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Text(
+              'name',
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
             ),
-          );
+          ),
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(primary: Color(0xff05445e)),
+              onPressed: () {},
+              child: Text('프로필 수정')),
+        ],
+      ),
+    );
   }
 }
---
+++ semomen/lib/pages/purchased_guide_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PurchasedGuidePage extends StatefulWidget {
+  PurchasedGuidePage({Key? key}) : super(key: key);
+
+  @override
+  State<PurchasedGuidePage> createState() => _PurchasedGuidePageState();
+}
+
+class _PurchasedGuidePageState extends State<PurchasedGuidePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
