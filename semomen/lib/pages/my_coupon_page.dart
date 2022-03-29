97720c5b69c1589c7de6c29c172dde3ed3db76f3
김형표 <gimhyeongpyo@gimhyeongpyoui-MacBookPro.local>
2022-03-29 21:52:40 +0900

    Create My coupon page

---
+++ semomen/lib/pages/my_coupon_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyCouponPage extends StatefulWidget {
+  MyCouponPage({Key? key}) : super(key: key);
+
+  @override
+  State<MyCouponPage> createState() => _MyCouponPageState();
+}
+
+class _MyCouponPageState extends State<MyCouponPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
