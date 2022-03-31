400a14ed5a481e5891cfd041dbdb98292ef3aa8d
gudvy88 <gudvy88@naver.com>
2022-03-31 18:11:05 +0900

    Add my coupon page ui and purchased guide page ui

--- semomen/lib/pages/my_coupon_page.dart
+++ semomen/lib/pages/my_coupon_page.dart
@@ -7,9 +7,108 @@ class MyCouponPage extends StatefulWidget {
   State<MyCouponPage> createState() => _MyCouponPageState();
 }
 
+//더미
+List<Map<String, dynamic>> dataList = [
+  {
+    "title": "가이드 구매 할인 쿠폰",
+    "subtitle": "5000원",
+    "subtitle1": "사용기간 2022/05/09까지"
+  },
+  {
+    "title": "가이드 구매 할인 쿠폰",
+    "subtitle": "30%",
+    "subtitle1": "사용기간 2022/04/09까지"
+  },
+];
+
 class _MyCouponPageState extends State<MyCouponPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      //앱바
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          '나의 쿠폰함',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black, //뒤로가기 색변경
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (BuildContext context, int index) {
+                Map<String, dynamic> data = dataList[index];
+                String title = data["title"];
+                String subtitle = data["subtitle"];
+                String subtitle1 = data["subtitle1"];
+                return CouponList(data: data);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+//쿠폰 리스트
+class CouponList extends StatelessWidget {
+  Map<String, dynamic> data;
+  CouponList({Key? key, required this.data}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(7),
+      elevation: 3.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListTile(
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Icon(
+                Icons.confirmation_num_outlined,
+                color: Colors.blue[300],
+                size: 30,
+              ),
+            ),
+            title: Text(
+              data["title"],
+              style: TextStyle(color: Colors.grey, fontSize: 13),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  data["subtitle"],
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  data["subtitle1"],
+                  style: TextStyle(color: Colors.grey, fontSize: 13),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- semomen/lib/pages/purchased_guide_page.dart
+++ semomen/lib/pages/purchased_guide_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class PurchasedGuidePage extends StatefulWidget {
@@ -7,9 +8,164 @@ class PurchasedGuidePage extends StatefulWidget {
   State<PurchasedGuidePage> createState() => _PurchasedGuidePageState();
 }
 
+List<Map<String, dynamic>> jobs = [
+  {
+    'imageUrl': '',
+    'job': '항해사',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+  {
+    'imageUrl': '',
+    'job': '수의사',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+  {
+    'imageUrl': '',
+    'job': '부사제',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+  {
+    'imageUrl': '',
+    'job': '공군 장교',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+  {
+    'imageUrl': '',
+    'job': '약사',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+  {
+    'imageUrl': '',
+    'job': '피아니스트',
+    'name': '김ㅇㅇ 멘토',
+    'title': '어쩌구',
+  },
+];
+
 class _PurchasedGuidePageState extends State<PurchasedGuidePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      //앱바
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          '구매가이드',
+          style: TextStyle(color: Colors.black),
+        ),
+        iconTheme: IconThemeData(
+          color: Colors.black, //뒤로가기 색변경
+        ),
+      ),
+      //구매가이드 리스트
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 3.7 / 5),
+              itemCount: jobs.length,
+              itemBuilder: (context, index) {
+                Map<String, dynamic> data = jobs[index];
+                String imageUrl = jobs[index]['imageUrl']!;
+                String job = jobs[index]['job']!;
+                String name = jobs[index]['name']!;
+                String title = jobs[index]['title']!;
+                return GuidItem(Jobs: data);
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class GuidItem extends StatelessWidget {
+  Map<String, dynamic> Jobs;
+
+  GuidItem({Key? key, required this.Jobs}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        margin: EdgeInsets.only(
+          left: 5,
+          right: 5,
+          top: 10,
+          bottom: 5,
+        ),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 1,
+              spreadRadius: 1,
+            )
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              //이미지
+              child: Container(
+                height: 120,
+                width: double.infinity,
+                color: Colors.amber,
+              ),
+            ),
+            //소개란
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          Jobs["job"],
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          Jobs["title"],
+                        )
+                      ],
+                    ),
+                    Spacer(),
+                    Text(
+                      Jobs["name"],
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
