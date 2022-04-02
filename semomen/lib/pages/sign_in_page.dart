7af6a0def7b70b88db75b08b2d2e85668b63837c
jspark2201 <jspark2201@gmail.com>
2022-04-02 13:58:52 +0900

    Add sign in page ui

--- semomen/lib/pages/sign_in_page.dart
+++ semomen/lib/pages/sign_in_page.dart
@@ -1,1 +1,178 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:semomen/pages/constants/constant.dart';
 
+class SignInPage extends StatefulWidget {
+  const SignInPage({Key? key}) : super(key: key);
+
+  @override
+  _SignInPageState createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool isObscure = true;
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.portraitUp]);
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: [
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Container(
+                  height: size.height * 0.4,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [
+                          mainBlueGreen,
+                          mainBlueGrotto
+                        ],
+
+                    )
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('세상의 모든 멘토', style: TextStyle(color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.bold),),
+                        ],
+                      ),
+                      Text(
+                        '진로 고민\n해결 가이드',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(height: 10.0, color: Colors.grey[200],),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 12.0,),
+                        Text('이메일', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Row(
+                          children: [
+                            Icon(Icons.email_outlined, color: Colors.grey,),
+                            SizedBox(width: 8.0,),
+                            Expanded(
+                              child: TextField(
+                                controller: _emailController,
+                                cursorColor: Colors.grey,
+                                style: TextStyle(fontSize: 14.0),
+                                decoration: InputDecoration(
+                                  hintText: '이메일을 입력하세요.',
+                                  border: InputBorder.none,
+                                ),
+                                keyboardType: TextInputType.text,
+                                onChanged: (String text) {
+
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        SizedBox(height: 8.0,),
+                        Text('비밀번호', style: TextStyle(fontWeight: FontWeight.bold),),
+                        Row(
+                          children: [
+                            Icon(Icons.lock_outline, color: Colors.grey,),
+                            SizedBox(width: 8.0,),
+                            Expanded(
+                              child: TextField(
+                                controller: _passwordController,
+                                cursorColor: Colors.grey,
+                                obscureText: isObscure,
+                                enableSuggestions: false,
+                                autocorrect: false,
+                                style: TextStyle(fontSize: 14.0),
+                                decoration: InputDecoration(
+                                  hintText: '비밀번호를 입력하세요.',
+                                  border: InputBorder.none,
+                                ),
+                                keyboardType: TextInputType.text,
+                                onChanged: (String text) {
+
+                                },
+                              ),
+                            ),
+                            SizedBox(width: 8.0,),
+                            GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    isObscure = !isObscure;
+                                  });
+                                },
+                                child: isObscure?Icon(Icons.visibility_off_outlined, color: Colors.grey,):Icon(Icons.remove_red_eye_outlined, color: Colors.grey,)
+                            ),
+                          ],
+                        ),
+                        Divider(),
+                        SizedBox(height: 16.0,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('비밀번호를 잊어버리셨나요?', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
+                            Text(' 비밀번호 재설정', style: TextStyle(color: mainBlueGrotto, fontWeight: FontWeight.bold),),
+                          ],
+                        ),
+                        SizedBox(height: 16.0,),
+                        SizedBox(
+                          width: size.width,
+                          child: ElevatedButton(
+                              onPressed: () {
+                                //emailSignIn(_emailController, _passwordController);
+                              },
+                              style: ElevatedButton.styleFrom(
+                                  primary: mainNavyBlue
+                              ),
+                              child: Text('로그인')
+                          ),
+                        ),
+                        SizedBox(height: 16.0,),
+                        Divider(),
+                        SizedBox(height: 16.0,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text('계정이 없으신가요?', style: TextStyle(color: Colors.grey, fontWeight: FontWeight.bold),),
+                            GestureDetector(
+                                onTap: () {
+                                },
+                                child: Text(' 계정 생성하기', style: TextStyle(color: mainBlueGrotto, fontWeight: FontWeight.bold),)),
+                          ],
+                        ),
+                        SizedBox(height: 16.0,),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
