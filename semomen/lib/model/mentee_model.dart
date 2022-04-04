733449a6e8a1ebee3fbe6e4624a810d75e87372e
jspark2201 <jspark2201@gmail.com>
2022-04-04 16:51:57 +0900

    Create mentee model

---
+++ semomen/lib/model/mentee_model.dart
@@ -0,0 +1,26 @@
+class MenteeModel{
+  String uid;
+  List<String> mentorUid;
+  List<String> programId;
+  List<String> couponList;
+
+  MenteeModel({
+    required this.uid,
+    required this.mentorUid,
+    required this.programId,
+    required this.couponList,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'uid':uid,
+    'mentor_uid':mentorUid,
+    'program_id':programId,
+    'coupon_list':couponList,
+  };
+
+  MenteeModel.fromJson(Map<String, dynamic> json) :
+        uid = json['uid'] ?? '',
+        mentorUid = json['mentor_uid'] ?? '',
+        programId = json['program_id'] ?? '',
+        couponList = json['coupon_list'] ?? '';
+}
