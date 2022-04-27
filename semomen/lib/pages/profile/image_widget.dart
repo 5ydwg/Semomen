a2e67b3206338432b9b6956128ed8a2050f07327
gudvy88 <gudvy88@naver.com>
2022-04-27 10:51:32 +0900

    profile page update

---
+++ semomen/lib/pages/profile/image_widget.dart
@@ -0,0 +1,54 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// class ImageWidget extends StatelessWidget {
+//   final File image;
+//   final ValueChanged<ImageSource> onClicked;
+//   const ImageWidget({Key? key, required this.image, required this.onClicked})
+//       : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: Stack(
+//         children: [
+//           buildImage(context),
+//           Positioned(
+//             bottom: 0,
+//             right: 4,
+//             child: buildEditIcon(color),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+
+//   Widget buildImage(BuildContext context) {
+//     final imagePath = this.image.path;
+//     final image = imagePath.contains('https://')
+//         ? NetworkImage(imagePath)
+//         : FileImage(File(imagePath));
+
+//     return ClipOval(
+//       child: Material(
+//         color: Colors.transparent,
+//         child: Ink.image(
+//           image: image as ImageProvider,
+//           fit: BoxFit.cover,
+//           width: 160,
+//           height: 160,
+//           child: InkWell(onTap: () async {
+//             final source = await showImageSource(context);
+//             if (source == null) return;
+
+//             onClicked(source);
+//           }),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// Widget buildEditIcon(Color color) => buildCircle()
